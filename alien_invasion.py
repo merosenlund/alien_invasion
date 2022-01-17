85e06e8042de64c1a3cff70225e8598c6565e4e6
Mitch Rosenlund <merosenlund@gmail.com>
2022-01-17 10:05:23 -0800

    Refactored event loop to separate method

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,17 +22,21 @@ class AlienInvasion:
   def run_game(self):
     """Start the loop for the game."""
     while True:
-      # Watch for keyboard and mouse events.
-      for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-          sys.exit()
-
+      self._check_events()
       # Redraw the screen during each pass through the loop.
       self.screen.fill(self.settings.bg_color)
       self.ship.blitme()
 
       # Make the most recently drawn screen visible.
       pygame.display.flip()
+
+  def _check_events(self):
+    """Respond to keypresses and mouse events."""
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        sys.exit()
+
+
 
 if __name__ == '__main__':
   # Make a game instance, and run the game.
