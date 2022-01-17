4aabb3f7cfa3f16d0840d123105665c3a5799b82
Mitch Rosenlund <merosenlund@gmail.com>
2022-01-17 10:09:20 -0800

    Refactored screen updates to it's own method

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,40 +5,43 @@ import pygame
 from settings import Settings
 from ship import Ship
 
+
 class AlienInvasion:
-  """Overall class to manage game assets and behavior."""
+    """Overall class to manage game assets and behavior."""
 
-  def __init__(self) -> None:
-    """Initilize the game, and create game resources."""
-    pygame.init()
-    self.settings = Settings()
+    def __init__(self) -> None:
+        """Initilize the game, and create game resources."""
+        pygame.init()
+        self.settings = Settings()
 
-    self.screen = pygame.display.set_mode(
-      (self.settings.screen_width, self.settings.screen_height))
-    pygame.display.set_caption("Alien Invasion")
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
+        pygame.display.set_caption("Alien Invasion")
 
-    self.ship = Ship(self)
+        self.ship = Ship(self)
 
-  def run_game(self):
-    """Start the loop for the game."""
-    while True:
-      self._check_events()
-      # Redraw the screen during each pass through the loop.
-      self.screen.fill(self.settings.bg_color)
-      self.ship.blitme()
+    def run_game(self):
+        """Start the loop for the game."""
+        while True:
+            self._check_events()
+            self._update_screen()
 
-      # Make the most recently drawn screen visible.
-      pygame.display.flip()
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-  def _check_events(self):
-    """Respond to keypresses and mouse events."""
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        sys.exit()
+    def _update_screen(self):
+        """Update imaes on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 
-
-if __name__ == '__main__':
-  # Make a game instance, and run the game.
-  ai = AlienInvasion()
-  ai.run_game()
+if __name__ == "__main__":
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
