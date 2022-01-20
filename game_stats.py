1f5d4ba2b8098facc21c1184f92b4d81610d48a1
Mitch Rosenlund <merosenlund@gmail.com>
2022-01-19 17:13:16 -0800

    Refactoring with black

--- game_stats.py
+++ game_stats.py
@@ -1,5 +1,6 @@
 import json
 
+
 class GameStats:
     """Track statistics for Alien Invasion."""
 
@@ -21,20 +22,20 @@ class GameStats:
         self.level = 1
 
     def load_score(self) -> int:
-      """Load the high score if there is one."""
-      pass
-      try:
-        file_name = 'high_score.txt'
-        with open(file_name) as f:
-          high_score = json.load(f)
-      except FileNotFoundError:
-        # There isn't a current highscore so we can just move on.
-        high_score = 0
-      return high_score
+        """Load the high score if there is one."""
+        pass
+        try:
+            file_name = "high_score.txt"
+            with open(file_name) as f:
+                high_score = json.load(f)
+        except FileNotFoundError:
+            # There isn't a current highscore so we can just move on.
+            high_score = 0
+        return high_score
 
     def save_score(self):
-      """If the current score is the high score then save it."""
-      pass
-      file_name = 'high_score.txt'
-      with open(file_name, 'w') as f:
-        json.dump(self.high_score, f)
+        """If the current score is the high score then save it."""
+        pass
+        file_name = "high_score.txt"
+        with open(file_name, "w") as f:
+            json.dump(self.high_score, f)
